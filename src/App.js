0c19ce58bdfe3378f3a022ec4a3054f9c5b513e5
Ekaraja <75461147+Ekaraja@users.noreply.github.com>
2022-07-28 18:32:34 +0200

    remove debugging borders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Order from './components/order/Order';
 import Navbar from './components/nav/Navbar';
 import Hero from './components/hero/Hero';
+import menu from "./assets/icons/menu.svg"
 import "./App.css"
 
 function App() {
   return (
     <div id="App">
-      <Navbar pageWrapId={"main"} outerContainerId={"nav"}/>
+      <Navbar pageWrapId={"main"} outerContainerId={"nav"} customBurgerIcon={ <img src={menu} alt="menu"/> } />
       <main id="main">
         <Hero/>
         <Order/>
